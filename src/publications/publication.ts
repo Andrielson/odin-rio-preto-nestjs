0dba06f5797e5e8637553b77ff473eb3ec2fc5a5
Andrielson Silva <andrielson@gmail.com>
2021-11-05 01:16:05 -0300

    Início do módulo de publicações

---
+++ src/publications/publication.ts
@@ -0,0 +1,5 @@
+export interface Publication {
+  keyword: string;
+  code: string;
+  link: string;
+}
---
+++ src/publications/publications.service.ts
@@ -0,0 +1,54 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { parse } from 'node-html-parser';
+import { from } from 'rxjs';
+import { concatMap, filter, map } from 'rxjs/operators';
+import { URLSearchParams } from 'url';
+import { Publication } from './publication';
+
+@Injectable()
+export class PublicationsService {
+  readonly #http!: HttpService;
+  readonly #apiUrl: string = 'https://www.riopreto.sp.gov.br/DiarioOficial';
+
+  constructor(httpService: HttpService) {
+    this.#http = httpService;
+  }
+
+  public findByKeywordAndDate(keyword: string, date: Date) {
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    };
+    const params = new URLSearchParams({
+      'Diario!listar.action': 'Buscar',
+      'diario.dataPublicacao': this.convertDateToStringInParamsFormat(date),
+    });
+    if (keyword !== '*') params.append('diario.palavraChave', keyword);
+
+    return this.#http
+      .post<Buffer>(`${this.#apiUrl}/Diario!listar.action`, params.toString(), {
+        headers,
+        responseType: 'arraybuffer',
+      })
+      .pipe(
+        map(({ data }) => parse(data.toString('utf-8'))),
+        concatMap((document) => from(document.getElementsByTagName('a'))),
+        filter((anchorElement) => anchorElement.hasAttribute('href')),
+        map((anchorWithHref) => anchorWithHref.getAttribute('href')!),
+        filter((href) =>
+          href.startsWith('Diario!arquivo.action?diario.codPublicacao='),
+        ),
+        map<string, Publication>((href) => ({
+          code: href.split('=')[1],
+          keyword,
+          link: `${this.#apiUrl}/${href}`,
+        })),
+      );
+  }
+
+  private convertDateToStringInParamsFormat(date: Date) {
+    const searchValue = /(\d{4})-(\d{2})-(\d{2}).*/;
+    const replaceValue = '$3/$2/$1';
+    return date.toISOString().replace(searchValue, replaceValue);
+  }
+}
