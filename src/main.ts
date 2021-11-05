c550559cffd444e7160c63e67920b7dc202b2052
Andrielson Silva <andrielson@gmail.com>
2021-11-05 01:16:46 -0300

    Testes dos módulos de inscritos e publicações

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import { PublicationsService } from './publications/publications.service';
+import { SubscribersService } from './subscribers/subscribers.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
-  const service = app.get(AppService);
-  const message = service.getHello();
+  const appService = app.get(AppService);
+  const message = appService.getHello();
   console.log({ message });
+
+  const subs = app.get(SubscribersService);
+  subs.findAll().subscribe((subscriber) => console.log({ subscriber }));
+
+  const pubs = app.get(PublicationsService);
+  pubs
+    .findByKeywordAndDate('semae', new Date('2021-11-04'))
+    .subscribe((publication) => console.log({ publication }));
+
   await app.close();
 }
 bootstrap();
