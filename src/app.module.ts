eea3bbda7c4807148b77eccb8ab4c33d65394475
Andrielson Silva <andrielson@gmail.com>
2021-11-05 01:16:32 -0300

    Criação do módulo de buscas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SubscribersModule } from './subscribers/subscribers.module';
+import { PublicationsModule } from './publications/publications.module';
+import { SearchesModule } from './searches/searches.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [SubscribersModule, PublicationsModule, SearchesModule],
   providers: [AppService],
 })
 export class AppModule {}
