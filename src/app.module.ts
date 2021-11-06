204cb230f175e39ab2b05b511fe253e3fa764704
Andrielson Silva <andrielson@gmail.com>
2021-11-06 16:41:02 -0300

    Novas funções no app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { SubscribersModule } from './subscribers/subscribers.module';
 import { PublicationsModule } from './publications/publications.module';
 import { SearchesModule } from './searches/searches.module';
+import { MailsModule } from './mails/mails.module';
 
 @Module({
-  imports: [SubscribersModule, PublicationsModule, SearchesModule],
+  imports: [SubscribersModule, PublicationsModule, SearchesModule, MailsModule],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { SearchesService } from './searches/searches.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  #searchService: SearchesService;
+  constructor(searchService: SearchesService) {
+    this.#searchService = searchService;
+  }
+  doIt(date: Date) {
+    return this.#searchService.searchByDate(date);
   }
 }
