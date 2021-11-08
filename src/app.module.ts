5ae31da5a02f2408df54f07e2fc7c31d45d1ecb4
Andrielson Silva <andrielson@gmail.com>
2021-11-07 21:42:46 -0300

    Limpeza no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { SubscribersModule } from './subscribers/subscribers.module';
-import { PublicationsModule } from './publications/publications.module';
-import { SearchModule } from './search/search.module';
-import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+import { SearchModule } from './search/search.module';
 
 const isGlobal = true;
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal }),
-    SubscribersModule,
-    PublicationsModule,
-    SearchModule,
-    MailModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal }), SearchModule],
   providers: [AppService],
 })
 export class AppModule {}
