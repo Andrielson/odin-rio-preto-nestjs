dc1129398a017c12dd30f86543fc517d04325008
Andrielson Silva <andrielson@gmail.com>
2021-11-07 21:43:26 -0300

    Utilização do serviço de autenticação

--- src/subscribers/subscribers.module.ts
+++ src/subscribers/subscribers.module.ts
@@ -1,10 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { SubscribersService } from './subscribers.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthModule],
   providers: [SubscribersService],
-  exports: [SubscribersService]
+  exports: [SubscribersService],
 })
 export class SubscribersModule {}
--- src/subscribers/subscribers.service.ts
+++ src/subscribers/subscribers.service.ts
@@ -1,21 +1,33 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { concatMap, from } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import { concatMap, from, tap } from 'rxjs';
+import { AuthService } from 'src/auth/auth.service';
 import { Subscriber } from './subscriber';
 
 @Injectable()
 export class SubscribersService {
   readonly #logger = new Logger(SubscribersService.name);
-  readonly #http!: HttpService;
-  readonly #apiUrl: string = 'http://web:3000/api/subscribers';
+  readonly #auth: AuthService;
+  readonly #config: ConfigService;
+  readonly #http: HttpService;
 
-  constructor(httpService: HttpService) {
-    this.#http = httpService;
+  constructor(auth: AuthService, config: ConfigService, http: HttpService) {
+    this.#auth = auth;
+    this.#config = config;
+    this.#http = http;
   }
 
   public findAll() {
-    return this.#http
-      .get<Subscriber[]>(this.#apiUrl)
-      .pipe(concatMap(({ data }) => from(data.map((it) => Object.freeze(it)))));
+    const url = this.#config
+      .get<string>('API_URL', 'http://web:3000/api')
+      .concat('/subscribers');
+
+    return this.#auth.header$.pipe(
+      tap(() => this.#logger.log(`GET request to ${url}...`)),
+      concatMap((headers) => this.#http.get<Subscriber[]>(url, { headers })),
+      tap(() => this.#logger.log(`Processing response...`)),
+      concatMap(({ data }) => from(data.map((it) => Object.freeze(it)))),
+    );
   }
 }
